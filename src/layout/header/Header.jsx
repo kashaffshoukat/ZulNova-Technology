6f381a44b5f4c4edb7114b565f76cf90c2dc978a
Zubairkhawar-11 <Zubair@saeedantechpvt.com>
2024-09-23 15:14:30 +0500

    header

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  const handleLinkClick = () => {
+  const handleLinkClick = () => { 
     setIsMenuOpen(false);
   };
 
