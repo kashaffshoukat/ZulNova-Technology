f28ee58b47d17bd977c67b3bd8216d63fd763651
Daniyal1303 <daniyalahmad1303@gmail.com>
2024-10-01 15:36:36 +0500

    Changes in company sites

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -8,63 +8,16 @@ import TechnologyStack from "./TechnologyStack";
 import FAQs from "./FAQS";
 import GetInTouch from "../../components/GetInTouch/GetInTouch";
 import PortolioSlider from "./PortolioSlider";
-import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import { amir, amirbg, landingbg, landingbg2, Landingbg3 } from "../../assets";
+import { landingbg } from "../../assets";
 
 const Landing = () => {
   const getInTouchRef = useRef(null);
-
-  const scrollToGetInTouch = () => {
-    getInTouchRef.current.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  };
 
   return (
     <>
       <div className="overflow-hidden">
         <section className="bg-white">
-          {/* <div className="mx-auto flex flex-col lg:flex-row items-center"> */}
-          {/* <div className="lg:w-1/2 mb-10 lg:mb-0 flex flex-col items-start justify-start">
-              <h1 className="text-4xl text-primary mb-4 font-bold">
-                Web and App Development
-              </h1>
-              <h2 className="text-3xl mb-6 font-bold">Services Agency</h2>
-              <p className="text-3xl text-gray-800 mb-6">
-                Saeedan Technology -{" "}
-                <span className="text-primary">
-                  Custom Software Development Company
-                </span>
-              </p>
-              <p className="text-2xl text-gray-600 mb-6 font-semibold">
-                Want to see your app idea shine?
-              </p>
-              <p className="text-xl text-gray-600 mb-6">
-                Our expert developers transform your vision into stunning,
-                user-friendly apps. Click now to turn your dreams into reality!
-              </p>
-              <div>
-                <Button
-                  onClick={scrollToGetInTouch}
-                  icon={<FaArrowRightLong />}
-                >
-                  Schedule Free Consultation
-                </Button>
-              </div>
-            </div> */}
-
-          {/* <div className="lg:w-1/2">
-              <LazyLoadImage
-                src="/assets/homepage.webp"
-                effect="blur"
-                className="w-full h-auto object-cover"
-                alt={"Web and App Development Illustration"}
-              />
-            </div> */}
-
-
           <div className="relative flex flex-col gap-5 md:gap-7 h-[70vh] items-center justify-center">
             <div
               className="absolute inset-0 bg-cover bg-center overflow-hidden"
@@ -82,35 +35,20 @@ const Landing = () => {
 
             {/* Subheading */}
             <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl w-[90%] md:w-[80%] text-center text-white relative z-10">
-              Access elite software engineers with diverse expertise in 100+ technologies, working in your timezone
+              Access elite software engineers with diverse expertise in 100+
+              technologies, working in your timezone
             </p>
 
             {/* Button */}
             <div className="relative z-10">
               <Button
-                // onClick={scrollToGetInTouch}
                 icon={<FaArrowRightLong />}
-                className={'border-white text-white'}
+                className={"border-white text-white"}
               >
                 Schedule Free Consultation
               </Button>
             </div>
           </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
           <div className="px-20 py-12">
             <Benefits />
